30c42696f1c313062e3bb44630b338e36205d9d6
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-06-21 10:45:35 -0400

    Created .prettierrc.cjs for shared Prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'none',
+    singleQuote: true,
+    printWidth: 200,
+    tabWidth: 4,
+    plugins: ['@ianvs/prettier-plugin-sort-imports'],
+    importOrder: ['^[./]']
+};
